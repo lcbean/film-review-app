fcd3ae4bd44b260211aa9e8a4893938b66478e57
Lindsay Bean <lcbean@unc.edu>
2024-09-02 10:05:44 -0400

    initial commit

---
+++ components/Card.js
@@ -0,0 +1,9 @@
+export default function Card({children}) {
+    return (
+        <div className="bg-white shadow-lg shadow-gray-200 rounded-md mb-5">
+            {children}
+
+        </div>
+
+    );
+}
